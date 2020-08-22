05c0679199d7cd0c38d1c2f13c1cd9c7576c9d22
Jackson Oliver <44202374+applesthepi@users.noreply.github.com>
2020-08-22 15:40:58 -0400

    heap allocation

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,12 +1,14 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-use linked_list_allocator::LockedHeap;
+use linked_list::LinkedListAllocator;
 use x86_64::{
 	structures::paging::{
 		mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
 	},
 	VirtAddr,
 };
+
+pub mod linked_list;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -50,5 +52,25 @@ unsafe impl GlobalAlloc for Dummy {
 	}
 }
 
+pub struct Locked<A> {
+	inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+	pub const fn new(inner: A) -> Self {
+		Locked {
+			inner: spin::Mutex::new(inner),
+		}
+	}
+
+	pub fn lock(&self) -> spin::MutexGuard<A> {
+		self.inner.lock()
+	}
+}
+
+fn align_up(addr: usize, align: usize) -> usize {
+	(addr + align - 1) & !(align - 1)
+}
+
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![feature(abi_x86_interrupt)]
+#![feature(const_fn)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 #![feature(alloc_error_handler)]
